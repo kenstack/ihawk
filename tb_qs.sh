6cfab96652a7ddd522fe39ca0c14f763407566cf
kenstack <kenstack@proximusllc.com>
2014-11-23 00:08:04 -0500

    add tb_qs.sh

--- tb_qs.sh
+++ tb_qs.sh
@@ -4,13 +4,6 @@ set -x
 echo""
 echo "<html><head><title>INSCRIPT</title></head>"
 echo "<body>"
-#echo $QUERY_STRING | tee testq
-#tb="${QUERY_STRING//%20/ }"
-#echo $tb | tee testq2
-
-#QUERY_STRING="duration=30&rate=1.0&serial=808661&port=/dev/ttyUSB0&dir=/home/pi"
-#saveIFS=$IFS
-#IFS='=&'
 IFS='&'; set -f
 for i in $QUERY_STRING; do
     declare "$i"
@@ -18,8 +11,6 @@ done
 unset IFS
 
 
-#parm=($QUERY_STRING)
-#IFS=$saveIFS
 
 echo $duration
 echo $rate
@@ -33,7 +24,7 @@ $dir/decoding-carelink/bin/mm-temp-basals.py set --rate $rate --duration $durati
 
 sleep 2
 # press the esc key, so we can see the results on the pump
-$dir/decoding-carelink/bin/mm-press-key.py --port /dev/ttyUSB0 --serial 808661 esc 
+$dir/decoding-carelink/bin/mm-press-key.py --port $port --serial $serial esc 
 
 echo""
 echo "</body></html>"
