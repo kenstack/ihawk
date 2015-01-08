be7cc9dd669602b1c1854077394dbbe1b47d8505
Ben West <bewest@gmail.com>
2015-01-07 19:37:08 -0800

    this seams to do more or less the same thing

--- his2.sh
+++ his2.sh
@@ -20,19 +20,23 @@ unset IFS
 
 
 # get last 6 hours of data
-$dir/decoding-carelink/bin/mm-latest2.py 360 --init --port $port --serial  $serial | tee ldata > /dev/null
+# $dir/decoding-carelink/bin/mm-latest2.py 360 --init --serial  $serial | tee ldata > /dev/null
+# python mm-latest2.py 360 --init --serial  $serial | tee ldata > /dev/null
+mm-latest.py --serial $serial --init --rf-minutes 3 360 --parser-out history.json | tee ldata > /dev/null
+<<EOT
 sed -n -e 's/.*data://p' ldata 
 #grab just history
 sed -n "/.*bolusdata:/,/.*bolusdata:/p" ldata | tee part1 >/dev/null
 #remove markers
 sed -e 's/bolusdata://g' part1 > part2 
 #remove line feeds
-tr '\n' ' ' < part2
+EOT
+tr '\n' ' ' < history.json
 
 echo""
 echo "</body></html>"
-rm part1
-rm part2
+# rm part1
+# rm part2
 # have to erase files
 # leave ldata for debug purposes in case of app crash
 
